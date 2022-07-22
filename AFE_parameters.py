fd3778842f926778c36736ad802375844676fd71
Addanki-Hasith <98515078+Addanki-Hasith@users.noreply.github.com>
2022-07-22 15:15:13 +0530

    Create AFE_parameters.py

---
+++ AFE_parameters.py
@@ -0,0 +1,18 @@
+# phaseTimingScheme {STAGGER, HIGH_PRF_MODE, MAX_AMB_REJ, DIS_POST_AMB_MAX_AMB_REJ}
+
+phaseTiming_mode 				= None
+STAGGER_mode 					= 0
+HIGH_PRF_MODE_mode 				= 1
+MAX_AMB_REJ_mode 				= 2
+DIS_POST_AMB_MAX_AMB_REJ_mode 	= 3
+
+# external clock decimation factor
+external_clk_NoDecimation = 0	
+external_clk_Decimation_2 = 1
+external_clk_Decimation_4 = 2
+
+# clocking modes {internal , external , single shot , mixed clock modes}
+CLK_MODE_INT	= 0
+CLK_MODE_EXT 	= 1
+CLK_MODE_SS 	= 2
+CLK_MODE_MIX 	= 3
